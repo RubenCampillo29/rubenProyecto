bbf2ccf1a805c70a6d3851cc40e473db5b4ab0ca
ruben <rubensaorines@gmail.com>
2023-05-17 18:16:01 +0200

    "Botones grandes"

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,63 @@
+<style>
+    .card {
+        margin: 20px;
+        background-color: #9805;
+
+    }
+
+    nav {
+
+        display: flex;
+    }
+</style>
+
+
+
+
 
 
 <nav>
 
-<a href="{{route('facturas.index')}}" class="btn btn-primary">Facturas</a>
-<a href="{{route('clientes.index')}}" class="btn btn-primary">Clientes</a>
-<a href="{{route('productos.index')}}" class="btn btn-primary">Productos</a>
-<a href="{{route('DetalleFacturas.index')}}" class="btn btn-primary">Todos los detalles</a>
+
+
+
+    <div class="card" style="width: 18rem;">
+        <img src=" " class="card-img-top" alt="factura">
+        <div class="card-body">
+            <h5 class="card-title">Facturas</h5>
+            <p class="card-text"></p>
+            <a href="{{route('facturas.index')}}" class="btn btn-primary">ver</a>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Clientes</h5>
+            <p class="card-text"></p>
+            <a href="{{route('clientes.index')}}" class="btn btn-primary">ver</a>
+        </div>
+    </div>
+
+
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Productos</h5>
+            <p class="card-text"></p>
+            <a href="{{route('productos.index')}}" class="btn btn-primary">ver</a>
+        </div>
+    </div>
+
+
+
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Todos los detalles</h5>
+            <p class="card-text"></p>
+            <a href="{{route('DetalleFacturas.index')}}" class="btn btn-primary">ver</a>
+        </div>
+    </div>
 
 </nav>
-
